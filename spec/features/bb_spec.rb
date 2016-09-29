a037c7b4f46374dab8190ba9f7caa66045a6a97b
jfbookingbug <jfenton@bookingbug.com>
2016-09-29 15:38:52 +0100

    first test

--- spec/features/bb_spec.rb
+++ spec/features/bb_spec.rb
@@ -3,9 +3,10 @@ require "capybara"
 require "cucumber"
 
 describe "BookingBug Automation BootCamp", :sauce => true do
-	it "Should run inside of SauceRunner and test in da cloud!!!" do
-# Visits our widget
-	visit INSERTHERE
+  it "Should run inside of SauceRunner and test in da cloud!!!" do
+  visit('http://bespoke.bookingbug.com/qa-automation')
+  click_button('Register')
+  expect(page).to have_css('h2', text: 'Register')
 # End of current functionality for now.        
   end
 end
