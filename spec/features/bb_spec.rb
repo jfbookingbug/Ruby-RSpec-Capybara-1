b0456d59f3983c0da7f75283e178ef8762611a23
jfbookingbug <jfenton@bookingbug.com>
2016-09-29 15:52:44 +0100

    Update bb_spec.rb

--- spec/features/bb_spec.rb
+++ spec/features/bb_spec.rb
@@ -4,9 +4,15 @@ require "cucumber"
 
 describe "BookingBug Automation BootCamp", :sauce => true do
   it "Should run inside of SauceRunner and test in da cloud!!!" do
+    
+    ####Paste your capybara ruby in here
+    
   visit('http://bespoke.bookingbug.com/qa-automation')
   click_button('Register')
   expect(page).to have_css('h2', text: 'Register')
+    
+    
+    
 # End of current functionality for now.        
   end
 end
