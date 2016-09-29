f0d4a89044b417108c447484e905230538b5e526
jfbookingbug <jfenton@bookingbug.com>
2016-09-29 15:34:40 +0100

    Update bb_spec.rb

--- spec/features/bb_spec.rb
+++ spec/features/bb_spec.rb
@@ -5,16 +5,7 @@ require "cucumber"
 describe "BookingBug Automation BootCamp", :sauce => true do
 	it "Should run inside of SauceRunner and test in da cloud!!!" do
 # Visits our widget
-	visit 'http://bespoke.bookingbug.com/rbc/staging/new_customer.html?firstName=Test&lastName=Automation&mobile=5815000800'
-	expect(page).to have_content 'How would you like to connect'
-	find('.text-center.service-item', :text => 'Video Conference').click
-	sleep 10
-       	find_button('Confirm').click
-	find('i.rbc-icon-arrow-down.avail-text', match: :first).click
-        find('li.time-slot.ng-scope', match: :first).click   
-        find_button('Continue').click  
-        find_button('Confirm').click
-        #find('#loading_icon')
+	visit INSERTHERE
 # End of current functionality for now.        
   end
 end
